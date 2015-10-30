00a5c1323732cd7023da40d8c0914184902fb0ff
Jay McCarthy <jay.mccarthy@gmail.com>
2015-10-30 12:24:06 -0400

    simplify dbuffer

--- mcc/dbuffer.c
+++ mcc/dbuffer.c
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <sys/file.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "mcc.h"
@@ -14,14 +15,11 @@ int linenum = 0;
 
 /*----------------------------------------------------------------------*/
 
-#define READSIZE 1025
-#define BUFSIZE (2*(READSIZE+1))
-static char buf[BUFSIZE];
+static char *buf;
 static int beginlptr = 0;
 static int endlptr = 0;
 static int forwardptr = 0;
 static int lexemeptr = 0;
-static int fd;
 int eof= 0;
 
 /*----------------------------------------------------------------------*/
@@ -33,27 +31,60 @@ void initbuf(char *file)
 	void moveupline();
 	char *getlinex();
 
-	if(file == NULL){
-          fd = 0;
-	}else{
-	  fd = open(file, O_RDONLY);
-	}
-	if (fd >= 0)
-	{
-		int i;
-		buf[READSIZE] = EOF;
-		buf[BUFSIZE-1] = EOF;
-		if ((i = read(fd, buf, READSIZE))!=READSIZE)
-			buf[i] = EOF;
-		moveupline();
-		if ((debuglevel & scanbug_m) && (linenum >=0) && !eof)
-			fprintf(stderr,"%d:%s",linenum,getlinex());
-	}
-	else
-	{
-		printf("Error, could not open %s for reading\n",file);
+	if (file == NULL) {
+      int fd = 0;
+      int total_size = 0;
+      int buf_size = 2;
+      buf = malloc(buf_size);
+      const int READSIZE = 1024;
+      char localbuf[READSIZE+1];
+      
+      while (1) {
+        int rd = read(fd, localbuf, READSIZE);
+        int next_total_size = total_size + rd;
+        int next_buf_size = next_total_size + 2;
+        char *next_buf = malloc(next_buf_size);
+        if (next_buf == NULL) {
+          printf("Error, could not allocate buffer for stdin\n");
+          exit(1);
+        }
+        
+        memcpy(next_buf, buf, total_size);
+        memcpy(next_buf + total_size, localbuf, rd);
+        next_buf[next_total_size] = EOF;
+        next_buf[next_total_size+1] = 0;
+        free(buf);
+        
+        buf = next_buf;
+        total_size = next_total_size;
+        buf_size = next_buf_size;
+        
+        if (rd < READSIZE) {
+          break;
+        }
+      }
+	} else {
+	  int fd = open(file, O_RDONLY);
+      if (fd < 0) {
+        printf("Error, could not open %s for reading\n",file);
 		exit(1);
+      }
+      struct stat st;
+      fstat(fd, &st);
+      int size = st.st_size;
+      buf = malloc(size+1);
+      if (buf == NULL) {
+        printf("Error, could not allocate buffer for %s\n",file);
+		exit(1);
+      }
+      int rd = read(fd, buf, size);
+      buf[rd] = 0;      
 	}
+    
+    moveupline();
+    if ((debuglevel & scanbug_m) && (linenum >=0) && !eof)
+      fprintf(stderr,"%d:%s",linenum,getlinex());
+
 } /* end initbuf() */
 
 /*----------------------------------------------------------------------*/
@@ -62,13 +93,7 @@ int getcurpos()
 /* return the offset of the current position from the begining of the line */
 {
 	int pos;
-	if (beginlptr < forwardptr)
-		if ((forwardptr < READSIZE) || (beginlptr > READSIZE))
-			pos = forwardptr - beginlptr;
-		else 
-			pos = (forwardptr - beginlptr) - 1;
-	else
-		pos = BUFSIZE - (beginlptr - endlptr);
+    pos = (forwardptr - beginlptr) - 1;
 	return(pos);
 } 	/* end of getcurpos */
 
@@ -79,32 +104,10 @@ char *getlinex()
 {
 	char *str,*s;
 	int len;
-	if (beginlptr < endlptr)
-	{
-		if ((endlptr < READSIZE) || (beginlptr > READSIZE))
-		{
-			len = endlptr - beginlptr;
-			str = (char *) salloc(len+1);
-			strncpy(str,buf+beginlptr, len);
-			str[len]='\0';
-		}
-		else 
-		{
-			len = (endlptr - beginlptr) - 1;
-			str = (char *) salloc(len+1);
-			strncpy(str,buf+beginlptr,READSIZE - beginlptr);
-			strncat(str,buf+(READSIZE+1),endlptr - (READSIZE+1));
-			str[len]='\0';
-		} /* end if */
-	}
-	else
-	{
-		len = BUFSIZE - (beginlptr - endlptr);
-		str = (char *) salloc(len+1);
-		strncpy(str,buf+beginlptr, (BUFSIZE-1) - beginlptr);
-		strncat(str,buf, endlptr);
-		str[len]='\0';
-	} /* end if */
+    len = endlptr - beginlptr;
+    str = (char *) salloc(len+1);
+    strncpy(str,buf+beginlptr, len);
+    str[len]='\0';
 	for(s = str; *s; s++)
 		if (*s == '\t')
 			*s = ' ';
@@ -115,35 +118,17 @@ char *getlinex()
 
 void moveupline()
 {
-	int i;
 	char ch;
 	int oldptr = beginlptr;
 	beginlptr = endlptr;
 
-	do
-	{
-		if (buf[endlptr] == EOF )
-		{
-			if (endlptr==READSIZE)
-			{
-              if ((i = read(fd, buf+(READSIZE+1), READSIZE)) != READSIZE)
-                buf[(READSIZE+1)+i] = EOF;
-				++endlptr;
-			}
-			else if (endlptr == (BUFSIZE-1))
-			{
-				if ((i = read(fd, buf, READSIZE)) != READSIZE)
-					buf[i]=EOF;
-				endlptr = 0;
-			}
-			else
-			{
-				eof = 1;
-				linenum--;
-				beginlptr = oldptr;
-				break;
-			}
-		}
+	do {
+      if (buf[endlptr] == EOF ) {
+        eof = 1;
+        linenum--;
+        beginlptr = oldptr;
+        break;
+      }
 	} while ((ch = buf[endlptr++]) != '\n');
 	linenum++;
 }
@@ -155,13 +140,6 @@ char getbc()
 	the char it is pointing to */
 {
 	char ch = buf[forwardptr++];
-	if (buf[forwardptr] == EOF)
-		switch(forwardptr)
-		{
-        case (BUFSIZE-1):	forwardptr = -1;
-			case READSIZE:	forwardptr++;
-			default:	break;
-		}
 	return(ch);
 } /* end getbc() */
 
@@ -170,11 +148,7 @@ char getbc()
 void ungetbc()
 /* moves the forward pointer back to the last character pointed to */
 {
-	switch(--forwardptr)
-	{
-    case -1:	forwardptr = (BUFSIZE-1);
-		case READSIZE:	--forwardptr;
-	}
+  --forwardptr;
 } /* end ungetbc() */
 
 /*----------------------------------------------------------------------*/
@@ -185,32 +159,12 @@ char *getlex()
 {
 	char *str;
 	int len;
-	if (lexemeptr < forwardptr)
-	{
-		if ((forwardptr < READSIZE) || (lexemeptr > READSIZE))
-		{
-			len = forwardptr - lexemeptr;
-			str = (char *) salloc(len+1);
-			strncpy(str,buf+lexemeptr, len);
-			str[len]='\0';
-		}
-		else 
-		{
-			len = (forwardptr - lexemeptr) - 1;
-			str = (char *) salloc(len+1);
-			strncpy(str,buf+lexemeptr,READSIZE - lexemeptr);
-			strncat(str,buf+(READSIZE+1),forwardptr - (READSIZE+1));
-			str[len]='\0';
-		} /* end if */
-	}
-	else
-	{
-		len = BUFSIZE - (lexemeptr - forwardptr);
-		str = (char *) salloc(len+1);
-		strncpy(str,buf+lexemeptr, (BUFSIZE-1) - lexemeptr);
-		strncat(str,buf, forwardptr);
-		str[len]='\0';
-	} /* end if */
+
+    len = forwardptr - lexemeptr;
+    str = (char *) salloc(len+1);
+    strncpy(str,buf+lexemeptr, len);
+    str[len]='\0';
+	    
 	lexemeptr = forwardptr;
 	return(str);
 } /* end getlex() */
