7b77b3b2e9a609447bbc767bdc77b7c90190cb15
Jay McCarthy <jay.mccarthy@gmail.com>
2017-05-07 22:02:09 -0400

    Running fibonacci

--- rkt/mic1/main.rkt
+++ rkt/mic1/main.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require racket/match
          racket/list
+         racket/runtime-path
          syntax/parse/define
          (for-syntax racket/base
                      syntax/parse))
@@ -1053,10 +1054,9 @@
        (Latch/N Clock:1 pre-MIR MIR)
        (Cut/N MIR
               (reverse
-               (flatten
-                (list MIR:AMUX MIR:COND MIR:ALU MIR:SH
-                      MIR:MBR MIR:MAR MIR:RD MIR:WR
-                      MIR:ENC MIR:C MIR:B MIR:A MIR:ADDR))))
+               (list MIR:AMUX MIR:COND MIR:ALU MIR:SH
+                     MIR:MBR MIR:MAR MIR:RD MIR:WR
+                     MIR:ENC MIR:C MIR:B MIR:A MIR:ADDR)))
 
        (Decoder/N MIR:A Asel)
        (Decoder/N MIR:B Bsel)
@@ -1239,8 +1239,88 @@
           MIR:MBR MIR:MAR MIR:RD MIR:WR
           MIR:ENC MIR:C MIR:B MIR:A MIR:ADDR))
   (for-each write-number! fields ns)
-  (simulate! (Cut/N (reverse (flatten fields)) MIR))
+  (simulate! (Cut/N (reverse fields) MIR))
   (read-number MIR))
+
+(module+ test
+  (define (chk-μenc se en)
+    (define s (μencode se))
+    (define an (μwrite s))
+    (with-chk (['mode 'μenc]
+               ['se se]
+               ['s s]
+               ['an (number->string an 2)]
+               ['en (number->string en 2)])
+      (chk an en)))
+
+  (define FIB-MICRO-PROGRAM
+    '(;; START/0: mar := sp; d := (1) + sp; wr
+      (A NJ + NS NB MAR NR WR ENC D SP P1 0)
+      ;; 1: f = (1) + (1); wr
+      (A NJ + NS NB NA NR WR ENC F P1 P1 0)
+      ;; 2: mbr := (1); b := (1); mar := d; wr;
+      (A NJ A NS MBR MAR NR WR ENC B D P1 0)
+      ;; 3: e := f + (1); wr;
+      (A NJ + NS NB NA NR WR ENC E F P1 0)
+      ;; LOOP/4: a := b + a;
+      (A NJ + NS NB NA NR NW ENC A A B 0)
+      ;; 5: d := sp + f;
+      (A NJ + NS NB NA NR NW ENC D F SP 0)
+      ;; 6: mar :=  d; mbr := a; wr; if n then goto DONE;
+      (A JN A NS MBR MAR NR WR NC PC D A 11)
+      ;; 7: sp := e + sp; wr;
+      (A NJ + NS NB NA NR WR ENC SP SP E 0)
+      ;; 8: b := a + b;
+      (A NJ + NS NB NA NR NW ENC B B A 0)
+      ;; 9: mar := sp; mbr := b; wr; if n then goto DONE;
+      (A JN A NS MBR MAR NR WR NC PC SP B 11)
+      ;; 10: sp := d; goto LOOP; wr;
+      (A J! A NS NB NA NR WR ENC SP PC D 4)
+      ;; DONE/11: wr; rd; goto START;
+      (A J! + NS NB NA RD WR NC PC PC PC 0)))
+
+  (define FIB-MICRO-IMAGE
+    '(#; 0 #b00000000101111010010011000000000
+      #; 1 #b00000000001111110110011000000000
+      #; 2 #b00010001101110111101011000000000
+      #; 3 #b00000000001111101111011000000000
+      #; 4 #b00000000000110101010101100000000
+      #; 5 #b00000000000111011111001000000000
+      #; 6 #b00110001101000001101101000001011
+      #; 7 #b00000000001100100010111000000000
+      #; 8 #b00000000000110111011101000000000
+      #; 9 #b00110001101000000010101100001011
+      #;10 #b01110000001100100000110100000100
+      #;11 #b01100000011000000000000000000000))
+
+  (define FIB-MICRO-IMAGE-STR
+    '(#; 0 "00000000101111010010011000000000"
+      #; 1 "00000000001111110110011000000000"
+      #; 2 "00010001101110111101011000000000"
+      #; 3 "00000000001111101111011000000000"
+      #; 4 "00000000000110101010101100000000"
+      #; 5 "00000000000111011111001000000000"
+      #; 6 "00110001101000001101101000001011"
+      #; 7 "00000000001100100010111000000000"
+      #; 8 "00000000000110111011101000000000"
+      #; 9 "00110001101000000010101100001011"
+      #;10 "01110000001100100000110100000100"
+      #;11 "01100000011000000000000000000000"))
+
+  (with-chk (['mode "fib program encoding"])
+    (chk (lines->image FIB-MICRO-IMAGE-STR) FIB-MICRO-IMAGE)
+    (for ([expected FIB-MICRO-IMAGE]
+          [actual-se (in-list FIB-MICRO-PROGRAM)]
+          [i (in-naturals)])
+      (define actual-ns (μencode actual-se))
+      (define actual-n (μwrite actual-ns))
+      (with-chk (['actual-se actual-se]
+                 ['actual-ns actual-ns]
+                 ['actual-n actual-n]
+                 ['expected expected]
+                 ['i i])
+        (chk (number->string actual-n 2)
+             (number->string expected 2))))))
 
 (module+ test
   (define standard-reg-values
@@ -1433,7 +1513,33 @@
               (hasheq 'A 1 'PC 88)
               (cons (hasheq 'MPC 1 'Write? 1 'MAR 1 'MBR 88) (hasheq 1 68))
               (cons (hasheq 'MPC 2 'Write? 1) (hasheq 1 88)))
-  
+
+  ;; Test Fibonacci program
+  (chk-mic1μs
+   FIB-MICRO-PROGRAM
+   empty (hasheq 'SP 0 'A 0)
+   ;; Start
+   (cons (hasheq 'MPC 1 'Write? 1 'MAR 0 'D 1) (hasheq))
+   (cons (hasheq 'MPC 2 'Write? 1 'F 2) (hasheq 0 0))
+   (cons (hasheq 'MPC 3 'Write? 1 'MBR 1 'B 1 'MAR 1) (hasheq 0 0))
+   (cons (hasheq 'MPC 4 'Write? 1 'E 3) (hasheq 0 0 1 1))
+   ;; Loop (iteration 1)
+   (cons (hasheq 'MPC 5 'A 1) (hasheq 0 0 1 1))
+   (cons (hasheq 'MPC 6 'D 2) (hasheq 0 0 1 1))
+   (cons (hasheq 'MPC 7 'MAR 2 'MBR 1 'Write? 1) (hasheq 0 0 1 1))
+   (cons (hasheq 'MPC 8 'SP 3 'Write? 1) (hasheq 0 0 1 1 2 1))
+   (cons (hasheq 'MPC 9 'B 2) (hasheq 0 0 1 1 2 1))
+   (cons (hasheq 'MPC 10 'MAR 3 'MBR 2 'Write? 1) (hasheq 0 0 1 1 2 1))
+   (cons (hasheq 'MPC 4 'SP 2 'Write? 1) (hasheq 0 0 1 1 2 1 3 2))
+   ;; Loop (iteration 2)
+   (cons (hasheq 'MPC 5 'A 3) (hasheq 0 0 1 1 2 1 3 2))
+   (cons (hasheq 'MPC 6 'D 4) (hasheq 0 0 1 1 2 1 3 2))
+   (cons (hasheq 'MPC 7 'MAR 4 'MBR 3 'Write? 1)
+         (hasheq 0 0 1 1 2 1 3 2))
+   (cons (hasheq 'MPC 8 'SP 5 'Write? 1)
+         (hasheq 0 0 1 1 2 1 3 2 4 3)))
+
+  ;; xxx jump ahead to the end?
   )
 
 (define (debug-MIC1 s)
@@ -1471,9 +1577,7 @@
 
     (start! inform!)))
 
-(define (file->image p)
-  (local-require racket/file)
-  (define lines (file->lines p))
+(define (lines->image lines)
   (define line 0)
   (for/list ([l (in-list lines)]
              #:unless (char=? #\# (string-ref l 0)))
@@ -1489,12 +1593,22 @@
     (define n (bits->number bits))
     n))
 
-;; xxx individual microinstructions work correctly, but programs are
-;; not running correctly.
+(define (file->image p)
+  (local-require racket/file)
+  (lines->image (file->lines p)))
+
+(module+ test
+  (let ()
+    (define sim (make-MIC1-simulator FIB-MICRO-IMAGE empty 0 100))
+    (debug-MIC1 sim)
+    (define mem (simulator-mem sim))
+    (local-require racket/vector)
+    (chk (vector-copy mem 100 125)
+         (vector 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+                 610 987 1597 2584 4181 6765 10946 17711 28657 46368))))
 
 (module+ main
-  (require racket/runtime-path
-           racket/cmdline)
+  (require racket/cmdline)
 
   ;; xxx move this to testing system
   (define-runtime-path standard-prom-path
